80587324f140ceb7473e4c9aff3f18bd5b9f12c6
Vitalij Ryndin <crashmax.dev@gmail.com>
2026-04-10 18:07:55 +0300

    fix(config): set base path for relative asset resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "./",
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toISOString())
   },
